c138d7a938f89c2f80ea63b122a92dc5c5a0d8df
Veneta Gergova <v.gergova1@gmail.com>
2023-02-26 17:59:31 +0000

    Upload New File

---
+++ app.js
@@ -0,0 +1,32 @@
+const form = document.getElementById('game-form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const choice = formData.get('choice');
+
+  const response = await fetch('http://127.0.0.1:5000/play', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      choice
+    })
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+    alert(error.message);
+    return;
+  }
+
+  const data = await response.json();
+  const { user_choice, computer_choice, result: gameResult } = data;
+
+  result.textContent = `You chose ${user_choice}. Computer chose ${computer_choice}. You ${gameResult}.`
+  result.style.display = 'block';
+
+})
